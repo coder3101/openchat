00054d6f8dfc0f360169afddd786e1ae7cc65bab
coder3101 <ashar786khan@gmail.com>
2023-06-04 23:39:41 +0530

    Improve home page

--- openchat/src/lib/chat.ts
+++ openchat/src/lib/chat.ts
@@ -1,27 +1,27 @@
 import { server } from './config';
 
 export let join = async (handle: string): Promise<string> => {
-    let url = new URL('join', server);
-    let response = await fetch(url, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ handle: handle })
-    });
-    if (response.status != 200) {
-        throw 'Cannot join room, ' + (await response.text());
-    } else {
-        let js = await response.json();
-        return js['cid'];
-    }
+	let url = new URL('join', server);
+	let response = await fetch(url, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ handle: handle })
+	});
+	if (response.status != 200) {
+		throw 'Cannot join room, ' + (await response.text());
+	} else {
+		let js = await response.json();
+		return js['cid'];
+	}
 };
 
 export let onlineCount = async (topic: string): Promise<number> => {
-    let url = new URL('online', server);
-    let response = await fetch(url);
-    if (response.status != 200) {
-        return -1;
-    } else {
-        let js = await response.json();
-        return js[topic];
-    }
-}
+	let url = new URL('online', server);
+	let response = await fetch(url);
+	if (response.status != 200) {
+		return -1;
+	} else {
+		let js = await response.json();
+		return js[topic];
+	}
+};
--- openchat/src/lib/message.ts
+++ openchat/src/lib/message.ts
@@ -1,19 +1,19 @@
 export interface ChatMessage {
-    timestamp: number;
-    body: string;
-    author: string;
+	timestamp: number;
+	body: string;
+	author: string;
 }
 
 export interface ChatEvent {
-    name: 'Joined' | 'Left';
-    identifier: string;
-    timestamp: number;
+	name: 'Joined' | 'Left';
+	identifier: string;
+	timestamp: number;
 }
 
 export let isChatMessage = (obj: any): obj is ChatMessage => {
-    return obj.timestamp !== undefined && obj.body !== undefined && obj.author !== undefined
-}
+	return obj.timestamp !== undefined && obj.body !== undefined && obj.author !== undefined;
+};
 
 export let isChatEvent = (obj: any): obj is ChatEvent => {
-    return obj.timestamp !== undefined && obj.name !== undefined && obj.identifier !== undefined
-}
+	return obj.timestamp !== undefined && obj.name !== undefined && obj.identifier !== undefined;
+};
