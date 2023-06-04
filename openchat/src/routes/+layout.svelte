3a465ddd935b7d1c59803861c84c1273ca676c88
Ashar <coder3101@users.noreply.github.com>
2023-06-05 00:25:17 +0530

    Make into a PWA (#3)

--- openchat/src/routes/+layout.svelte
+++ openchat/src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script>
 	import '../app.postcss';
+	import { pwaInfo } from 'virtual:pwa-info';
+
+	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 </script>
 
 <svelte:head>
 	<title>Openchat - Chat with anyone, anywhere!</title>
+	{@html webManifestLink}
 </svelte:head>
 
 <div class="min-h-screen">
--- openchat/vite.config.ts
+++ openchat/vite.config.ts
@@ -1,6 +1,42 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			srcDir: './src',
+			mode: 'production',
+			scope: '/',
+			base: '/',
+			selfDestroying: process.env.SELF_DESTROYING_SW === 'true',
+			manifest: {
+				short_name: 'Openchat',
+				name: 'Openchat',
+				start_url: '/',
+				scope: '/',
+				display: 'standalone',
+				theme_color: '#ffffff',
+				background_color: '#581c87',
+				icons: [
+					{ src: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+					{ src: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' }
+				]
+			},
+			injectManifest: {
+				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+			},
+			workbox: {
+				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+			},
+			devOptions: {
+				enabled: true,
+				type: 'module',
+				navigateFallback: '/'
+			},
+			// if you have shared info in svelte config file put in a separate module and use it also here
+			kit: {}
+		})
+	]
 });
